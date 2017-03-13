36edc9bd108b22dd51e1ef96fcbecf7811c142df
Knut Ahlers <knut@ahlers.me>
2017-03-13 13:34:42 +0100

    Add a healthcheck URL
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- run.go
+++ run.go
@@ -28,6 +28,7 @@ func init() {
 func runLocally(cmd *cobra.Command, args []string) error {
 	r := mux.NewRouter()
 	r.HandleFunc("/{user}/{repo}/releases/download/{version}/{binary}", handleLocalExecution)
+	r.HandleFunc("/status", func(res http.ResponseWriter, r *http.Request) { res.WriteHeader(http.StatusNoContent) })
 
 	log.Printf("Starting local webserver on %s", executionPort)
 	return http.ListenAndServe(executionPort, r)
